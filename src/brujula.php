7cb334594c64f4d0f637d4b00295afd8035f82c5
Herbert Orellana <ao43085@gmail.com>
2023-08-26 10:23:49 -0600

    Menu principal
    Menu principal

---
+++ src/brujula.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="output.css" rel="stylesheet">
+</head>
+<body>
+    <main class="bg-[#3068D8] bg-gradient-to-r from-[#3068D8] to-indigo-900 pt-5 w-[375px] h-screen">
+      <div class="flex items-center justify-between p-5">
+        <div>
+          <p class=" text-lg font-bold text-white"> HOLA DIEGO !</p>
+        </div>
+        <div class="flex items-center justify-between gap-2">
+          <div>
+            <img class="" src="/imgs/bx-log-out.svg" alt="">
+          </div>
+          <div>
+            <p class="text-[#FEB1B1]">Salir</p>
+          </div>
+        </div>        
+      </div>
+      <div class="flex flex-col gap-10 mt-10">
+        <div class="w-[280px] h-[100px] bg-[#D9D9D9] ml-[12%] rounded-b-[50px] rounded-r-[50px] flex items-center">
+          <div class="pl-12 w-[110px] text-[#577CBE]">
+            <p>Modifica tu perfil</p>
+          </div>
+          <div class="pl-12">
+            <img class="" src="/imgs/perfil-home.svg" alt="">
+          </div>
+        </div>
+        <div class="w-[280px] h-[100px] bg-[#577CBE] ml-[12%] rounded-b-[50px] rounded-r-[50px] flex items-center">
+            <div class="pl-12 w-[150px] text-white">
+              <p>Planifica tus metas</p>
+            </div>
+            <div>
+              <img class="" src="/imgs/metas-home.svg" alt="">
+            </div>
+        </div>
+        <div class="w-[280px] h-[100px] bg-[#D9D9D9] ml-[12%] rounded-b-[50px] rounded-r-[50px] flex items-center">
+          <div class="pl-12 w-[150px] text-[#577CBE]">
+            <p>Verifica tu progreso</p>
+          </div>
+          <div>
+            <img class="" src="/imgs/progreso-home.svg" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center justify-between pl-6 pr-6 mt-16">
+        <img class="" src="/imgs/bx-notepad1.svg" alt="">
+        <img class="" src="/imgs/bx-home-alt-2.svg" alt="">
+        <img class="" src="/imgs/bx-user.svg" alt="">
+      </div>  
+    </main>
+</body>
