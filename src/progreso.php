a756d38f22c6c4873d11206fb1f674586b4b355f
Herbert Orellana <ao43085@gmail.com>
2023-08-28 16:12:29 -0600

    Task verify your progress
    Task verify your progress

---
+++ src/progreso.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="output.css" rel="stylesheet">
+</head>
+<body>
+    <main class="bg-[#3068D8] bg-gradient-to-r from-[#3068D8] to-indigo-900 pt-5 w-[375px] h-[1050px]">
+      <div class="flex items-center justify-start p-5">
+        <div class="flex items-center justify-between gap-2">
+          <div>
+            <img class="" src="/imgs/top-left-Vector.svg" alt="">
+          </div>
+        </div>            
+        <div>
+          <p class=" text-lg font-bold text-white">VERIFICA TU PROGRESO</p>
+        </div>    
+      </div>
+      <div class="w-[300px] h-[40px] ml-[8%] rounded-full flex items-center justify-around border-2 border-white">
+        <div>
+          <img src="/imgs/header-left-vector.svg" alt="">
+        </div>
+        <div>
+          <p class="text-white">2023</p>
+        </div>
+        <div>
+          <img class="" src="/imgs/header-rigth-vector.svg" alt="">
+        </div>
+      </div>
+      <div class="flex flex-col gap-3 mt-10">
+        <div>
+            <div class="w-[300px] h-[50px] bg-[#7030A0] ml-[8%] rounded-full text-white flex items-center justify-center">
+                <p class="font-semibold text-lg">ESPIRITUAL</p>
+            </div>
+            <ul class="text-white font-semibold text-lg flex flex-col gap-3 list-disc ml-[10%] pr-10 mt-5">
+                <li>
+                    <div class="flex items-center justify-between">
+                        <p>PROCESO DE CONVERSION</p>
+                        <p>5/5</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="flex items-center justify-between">
+                        <p>SER UN DISCIPULO</p>
+                        <p>5/5</p>
+                    </div>
+                </li>                
+            </ul>
+        </div>
+        <div>
+            <div class="w-[300px] h-[50px] bg-[#FFC000] ml-[8%] rounded-full text-white flex items-center justify-center">
+                <p class="font-semibold text-lg">FAMILIAR</p>
+            </div>
+            <ul class="text-white font-semibold text-lg flex flex-col gap-3 list-disc ml-[10%] pr-10 mt-5">
+                <li>
+                    <div class="flex items-center justify-between">
+                        <p>ESPOSO/NOVIO</p>
+                        <p>5/5</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="flex items-center justify-between">
+                        <p>PADRE</p>
+                        <p>5/5</p>
+                    </div>
+                </li> 
+                <li>
+                    <div class="flex items-center justify-between">
+                        <p>HIJO</p>
+                        <p>5/5</p>
+                    </div>
+                </li>                                
+            </ul>
+        </div>   
+        <div>
+            <div class="w-[300px] h-[50px] bg-[#00B050] ml-[8%] rounded-full text-white flex items-center justify-center">
+                <p class="font-semibold text-lg">INTELECTUAL</p>
+            </div>
+            <ul class="text-white font-semibold text-lg flex flex-col gap-3 list-disc ml-[10%] pr-10 mt-5">
+                <li>
+                    <div class="flex items-center justify-between">
+                        <p>EDUCACION FORMAL</p>
+                        <p>5/5</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="flex items-center justify-between">
+                        <p>AUTOEDUCACION</p>
+                        <p>5/5</p>
+                    </div>
+                </li>                
+            </ul>
+        </div>    
+        <div>
+            <div class="w-[300px] h-[50px] bg-[#00B0F0] ml-[8%] rounded-full text-white flex items-center justify-center">
+                <p class="font-semibold text-lg">LABORAL</p>
+            </div>
+            <ul class="text-white font-semibold text-lg flex flex-col gap-3 list-disc ml-[10%] pr-10 mt-5">
+                <li>
+                    <div class="flex items-center justify-between">
+                        <p>EMPLEADO</p>
+                        <p>5/5</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="flex items-center justify-between">
+                        <p>EMPRENDIMIENTO</p>
+                        <p>5/5</p>
+                    </div>
+                </li>                
+            </ul>
+        </div>      
+        <div>
+            <div class="w-[300px] h-[50px] bg-[#FF0000] ml-[8%] rounded-full text-white flex items-center justify-center">
+                <p class="font-semibold text-lg">FISICO</p>
+            </div>
+            <ul class="text-white font-semibold text-lg flex flex-col gap-3 list-disc ml-[10%] pr-10 mt-5">
+                <li>
+                    <div class="flex items-center justify-between">
+                        <p>EJERCICIOS</p>
+                        <p>5/5</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="flex items-center justify-between">
+                        <p>HOBBIES</p>
+                        <p>5/5</p>
+                    </div>
+                </li>                
+            </ul>
+        </div>                        
+      </div>
+      <div class="flex items-center justify-between pl-6 pr-6 mt-12">
+        <img class="" src="/imgs/bx-notepad1.svg" alt="">
+        <img class="" src="/imgs/bx-home-alt-2.svg" alt="">
+        <img class="" src="/imgs/bx-user.svg" alt="">
+      </div>  
+    </main>
+</body>
